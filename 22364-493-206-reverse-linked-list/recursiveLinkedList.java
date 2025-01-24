f10e9211cea607ac1e0858cbca8664cd7679f2c9
J. Sebastian Perez <sebastian_perez95@hotmail.com>
2025-01-24 10:23:38 -0500

    recursive linked list

---
+++ 22364-493-206-reverse-linked-list/recursiveLinkedList.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null || head.next == null) return head;
+        
+        ListNode temp = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return temp;
+    }
+}
